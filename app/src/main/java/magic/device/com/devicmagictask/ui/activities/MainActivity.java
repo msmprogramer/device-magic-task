ecdfc51d757a1de99b6498bc519a61a54e97cce9
msalama <msalama@apptitude-eg.com>
2016-04-28 16:51:43 +0200

    init device magic task project

---
+++ app/src/main/java/magic/device/com/devicmagictask/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package magic.device.com.devicmagictask.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import magic.device.com.devicmagictask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
